1b821e91b8b618149ed929e20f3abc15fc269dcb
PriyanshuPilkhwal <priyanshupilkhwal024@gmail.com>
2025-10-20 12:40:37 +0530

    Initial commit: Add GameStore project files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { App } from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -13,7 +13,7 @@ async function enableMocking() {
   // can run in the "preview" environment as well.
   // This allows us to test the production build locally with mock data.
   
-  const { worker } = await import('./mocks/browser')
+  const { worker } = await import('./mocks/browser.tsx')
  
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is ready.
